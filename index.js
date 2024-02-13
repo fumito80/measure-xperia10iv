caee73927f14cb14f8621da352ce663bea62286e
fumito80 <fumito80@gmail.com>
2024-02-13 16:23:10 +0900

    support multi-models

--- index.js
+++ index.js
@@ -1,9 +1,26 @@
-document.body.style.zoom = '168.4%';
+const zooms = {
+  A202SO: '168.4%',
+};
 
 const divs = 150;
 
-function initialize() {
-  const $main = document.querySelector('.main');
+async function setZoom() {
+  const { model } = await navigator.userAgentData.getHighEntropyValues(['model']).catch(() => ({}));
+  const zoom = zooms[model];
+  if (!zoom) {
+    return false;
+  }
+  document.body.style.zoom = zoom;
+  return true;
+}
+
+async function initialize() {
+  const supported = await setZoom();
+  if (!supported) {
+    document.body.textContent = 'This model is not supported.';
+    return;
+  }
+  const [$main] = document.getElementsByClassName('main');
   $main?.append(...[...Array(divs)].map(() => document.createElement('div')));
 }
 
