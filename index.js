682ee0088204762d08e9b93ea15e88b4ef8f24b6
fumito80 <fumito80@gmail.com>
2024-02-13 11:18:13 +0900

    v1.2

--- index.js
+++ index.js
@@ -1,17 +1,10 @@
-function $(selector, parent = document) {
-  return parent.querySelector(selector);
+document.body.style.zoom = '168.4%';
+
+const divs = 150;
+
+function initialize() {
+  const $main = document.querySelector('.main');
+  $main?.append(...[...Array(divs)].map(() => document.createElement('div')));
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-  document.body.style.zoom = '168.4%';
-  const $main = $('.main');
-  for (let i = 0; i < 150; i += 1) {
-    const $div = $main.appendChild(document.createElement('div'));
-    if (i % 10 === 0) {
-      $div.classList.add('just');
-      $div.setAttribute('title', Math.trunc(i / 10));
-    } else if (i % 5 === 0) {
-      $div.classList.add('half');
-    }
-  }
-});
+document.addEventListener('DOMContentLoaded', initialize);
