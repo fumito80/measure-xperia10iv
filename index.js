ae0329a0c09ba1a484f549a81545bca20a061149
fumito80 <fumito80@gmail.com>
2024-11-14 19:33:42 +0900

    PWA

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ function initialize() {
 }
 
 document.addEventListener('DOMContentLoaded', initialize);
+
+// for PWA
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('sw.js')
+    .then((registration) => {
+      console.info('ServiceWorker registration successful with scope: ', registration.scope);
+      registration.addEventListener('updatefound', () => {
+        registration.update();
+        console.info('PWA Registration update.');
+      });
+    })
+    .catch((err) => console.info('ServiceWorker registration failed: ', err));
+}
